378b8f5c1669dbdbcfcd5adc5ba1daf15a4c3bf6
Ankur Semle <semleankur@gmail.com>
2024-02-26 01:54:33 +0530

    update form backend

--- actions/form.ts
+++ actions/form.ts
@@ -5,41 +5,6 @@ class UserNotFoundErr extends Error {}
 import prisma from "@/lib/prisma";
 import { formSchemaType, formSchema } from "@/schemas/form";
 
-export async function GetFormStats() {
-  const user = await currentUser();
-  if (!user) {
-    throw new UserNotFoundErr();
-  }
-
-  const stats = await prisma.form.aggregate({
-    where: {
-      userId: user.id,
-    },
-    _sum: {
-      visits: true,
-      submissions: true,
-    },
-  });
-
-  const visits = stats._sum.visits || 0;
-  const submissions = stats._sum.submissions || 0;
-
-  let submissionRate = 0;
-
-  if (visits > 0) {
-    submissionRate = (submissions / visits) * 100;
-  }
-
-  const bounceRate = 100 - submissionRate;
-
-  return {
-    visits,
-    submissions,
-    submissionRate,
-    bounceRate,
-  };
-}
-
 export async function CreateForm(data: formSchemaType) {
   const user = await currentUser();
   const validation = formSchema.safeParse(data);
